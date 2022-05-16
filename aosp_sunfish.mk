8f49d646af35ae908703efbb290d4f88347a6f5c
Michael Bestas <mkbestas@lineageos.org>
2022-05-16 14:25:18 +0200

    Update fingerprint/build description from SP2A.220505.002
    Change-Id: If2398ef57555a887f333fc36c4e6c0f11dec2cce

--- aosp_sunfish.mk
+++ aosp_sunfish.mk
@@ -74,8 +74,8 @@ PRODUCT_MODEL := Pixel 4a
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sunfish \
-    PRIVATE_BUILD_DESC="sunfish-user 12 SP2A.220405.003 8210211 release-keys"
+    PRIVATE_BUILD_DESC="sunfish-user 12 SP2A.220505.002 8353555 release-keys"
 
-BUILD_FINGERPRINT := google/sunfish/sunfish:12/SP2A.220405.003/8210211:user/release-keys
+BUILD_FINGERPRINT := google/sunfish/sunfish:12/SP2A.220505.002/8353555:user/release-keys
 
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
